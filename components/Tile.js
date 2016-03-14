8f3f177e6866573b5a28b49af982ba948cc8d402
Dan Michaelis <dmichaelis0@gmail.com>
2016-03-13 19:18:11 -0700

    randomly visualize units

--- components/Tile.js
+++ components/Tile.js
@@ -13,10 +13,13 @@ export default class Tile extends Component {
         return <div key={y}> {
                 row.map((squareStatus, x) => {
                   let color = ''
+                  let unit = squareStatus > 0;
                   if(squareStatus === -1) {
                     color = 'rgba(0,0,0,0.5)';
                   }
-                  return <div key={x} style={{border:'1px solid black', marginBottom: '-4px', height: '30', width: '30', display: 'inline-block', backgroundColor: color}}></div>
+                  return <div key={x} style={{border:'1px solid black', marginBottom: '-4px', height: '30', width: '30', display: 'inline-block', backgroundColor: color}}>
+                    {squareStatus > 0 ? <div className='unit' style={{backgroundColor: 'black', height: '25', width: '25', position: 'absolute', textAlign: 'center', display: 'inline-block', color: 'white', borderRadius: '50%'}}>{squareStatus}</div>: ''}
+                  </div>
                 })}
               </div>
       })
--- reducers/board.js
+++ reducers/board.js
@@ -1,11 +1,12 @@
 import Immutable from 'immutable';
 import {handleActions} from 'redux-actions'
 import _ from 'lodash';
+let unitId = 1
 function generateBoard(height=5, width=5) {
   return Array(height).fill().map(() => Array(width).fill().map(() => generateTile()));
 }
 function generateTile() {
-  return {ownerId: -1, squareLayout: Array(3).fill().map(() => Array(3).fill().map(() => _.sample([0, 0, 0, 0, -1]))) };
+  return {ownerId: -1, squareLayout: Array(3).fill().map(() => Array(3).fill().map(() => _.sample([0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, -1, unitId++]))) };
 }
 
 const initialState = {
